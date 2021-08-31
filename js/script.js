b3b53b50f0c2c93e934a269064262c67045083bd
Tai <taigea488@gmail.com>
2021-08-31 18:38:50 +0800

    upload

--- js/script.js
+++ js/script.js
@@ -2,28 +2,28 @@ var h = document.documentElement.scrollTop || document.body.scrollTop;
 var bar = document.querySelectorAll(".top-nav");
 var p = document.querySelectorAll("p.left");
 
-//
+
 // window.onscroll = function () {
 //
 //     if (window.pageYOffset >= 30) {
-//         bar[0].style.backgroundColor = "blueviolet"
+//         bar[0].style.backgroundColor = "grey"
 //         p[0].style.color = "black";
 //     } else if (window.pageYOffset === 0) {
 //         bar[0].style.backgroundColor = "transparent"
-//         p[0].style.color = "blueviolet";
+//         p[0].style.color = "white";
 //     }
 // }
 
 
-var a = document.querySelectorAll(".tab ul li a");
-for (let i = 0; i < a.length; i++) {
-    a[i].onclick = function () {
-        for (let j = 0; j < a.length; j++) {
-            a[j].removeAttribute("class", "active");
-        }
-        a[i].setAttribute("class", "active");
-    }
-}
+// var a = document.querySelectorAll(".tab ul li a");
+// for (let i = 0; i < a.length; i++) {
+//     a[i].onclick = function () {
+//         for (let j = 0; j < a.length; j++) {
+//             a[j].removeAttribute("class", "active");
+//         }
+//         a[i].setAttribute("class", "active");
+//     }
+// }
 
 
 //
